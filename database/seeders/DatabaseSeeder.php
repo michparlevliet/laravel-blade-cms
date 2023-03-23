584a30333a25e1bacb5a994339ee19b11a8e0430
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-03-23 07:05:21 -0400

    add new table (entry)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Type;
 use App\Models\Project;
+use App\Models\Entry;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,10 +21,13 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Type::truncate();
         Project::truncate();
+        Entry::truncate(); 
         
         User::factory()->count(2)->create();
         Type::factory()->count(3)->create();
         Project::factory()->count(4)->create();
+        
+        Entry::factory()->count(4)->create();
             
     }
 }
