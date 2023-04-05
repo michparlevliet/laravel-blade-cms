cee9191b2ff900664a0c7bcfef87370317a25a4a
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-04-05 15:35:10 -0400

    added entries CRUD and views

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Models\Project;
+
 use App\Http\Controllers\ConsoleController;
 use App\Http\Controllers\ProjectsController;
 use App\Http\Controllers\TypesController;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\EntriesController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -57,3 +60,10 @@ Route::post('/console/types/add', [TypesController::class, 'add'])->middleware('
 Route::get('/console/types/edit/{type:id}', [TypesController::class, 'editForm'])->where('type', '[0-9]+')->middleware('auth');
 Route::post('/console/types/edit/{type:id}', [TypesController::class, 'edit'])->where('type', '[0-9]+')->middleware('auth');
 Route::get('/console/types/delete/{type:id}', [TypesController::class, 'delete'])->where('type', '[0-9]+')->middleware('auth');
+
+Route::get('/console/entries/list', [EntriesController::class, 'list'])->middleware('auth');
+Route::get('/console/entries/add', [EntriesController::class, 'addForm'])->middleware('auth');
+Route::post('/console/entries/add', [EntriesController::class, 'add'])->middleware('auth');
+Route::get('/console/entries/edit/{entry:id}', [EntriesController::class, 'editForm'])->where('entry', '[0-9]+')->middleware('auth');
+Route::post('/console/entries/edit/{entry:id}', [EntriesController::class, 'edit'])->where('entry', '[0-9]+')->middleware('auth');
+Route::get('/console/entries/delete/{entry:id}', [EntriesController::class, 'delete'])->where('entry', '[0-9]+')->middleware('auth');
